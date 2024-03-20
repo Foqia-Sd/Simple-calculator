6c661cd572515a81f878ab730f98a594b02fa5fd
Foqia-Sd <160354822+Foqia-Sd@users.noreply.github.com>
2024-03-20 08:07:02 -0700

    rav5ad_live_simple-calculator

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+   { message: "Enter your first number", type: "number", name: "firstNumber" },
+   { message: "Enter your second number", type: "number", name: "secondNumber" },
+   { message: "select one operator to perform action",
+       type: "list", name: "operator",
+        choices: ["ADDITION", "SUBRACTION", "MULTIPLICATION", "DIVISION"],
+   },
+]);
+//Conditional statements
+if (answer.operator === "ADDITION") {
+   console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "SUBRACTION") {
+   console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+   console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "DIVISION") {
+   console.log(answer.firstNumber / answer.secondNumber);
+} else { ("plese let correct operator");
+ }
+ 
+
+ 
+
